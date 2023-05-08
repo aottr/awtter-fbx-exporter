4dd1d339b241956749111ca522e64dd0eff4dd0c
Alex <alex@tailbyte.org>
2023-05-08 23:42:21 +0200

    add model select

--- awex/main.py
+++ awex/main.py
@@ -1,5 +1,6 @@
 from awex.lib.sdaclient import SdaClient
 import typer
+from click import Choice
 import sys
 from rich.progress import Progress, SpinnerColumn, TextColumn
 
@@ -22,6 +23,24 @@ def main():
         sys.exit(1)
 
     print("Successfully logged in!")
+    models = None
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Fetching models...", total=None)
+        try:
+            models = client.get_models()
+        except Exception as e:
+            print(e)
+
+    print("Available Models")
+    for i in range(len(models)):
+        print(f"{i}\t{models[i]['name']}")
+    click_choice = Choice([str(i) for i in range(len(models))])
+    model_prompt = typer.prompt("Which model", "0", show_choices=True, type=click_choice)
 
 
 typer.run(main)
