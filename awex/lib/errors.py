2ed9f338833a84484be30e27f8d2fd3ddc1589df
Alex <alex@tailbyte.org>
2023-05-08 20:09:52 +0200

    add comments

--- awex/lib/errors.py
+++ awex/lib/errors.py
@@ -1,3 +1,5 @@
 class InvalidCredentialsError(Exception):
+    """Exception raised for providing the wrong username / password.
+    """
     def __init__(self):
         super().__init__('The provided login credentials are not correct.')
