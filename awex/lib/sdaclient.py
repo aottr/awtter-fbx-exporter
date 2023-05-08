b52c8eb4d8d050ad1a958b2289b647ac0fa006c3
Alex <alex@tailbyte.org>
2023-05-08 20:10:09 +0200

    add model retrieval

--- awex/lib/sdaclient.py
+++ awex/lib/sdaclient.py
@@ -5,6 +5,11 @@ from .errors import InvalidCredentialsError
 class SdaClient:
 
     def __init__(self, username: str, password: str):
+        """
+        Create a new API client for ShadeDoes3d
+        :param username: client username
+        :param password: client password
+        """
         res = requests.post(
             'https://shadedoes3d.com/api/auth',
             data={
@@ -19,4 +24,30 @@ class SdaClient:
         self.token = res.json()['data']['token']
 
     def get_models(self):
-        pass
+
+        models = []
+
+        res = requests.get(
+            'https://shadedoes3d.com/api/products',
+            headers={
+                'Authorization': f'Token {self.token}'
+            }
+        )
+
+        data = res.json()['data']
+        for product in data:
+            if not product['isBaseModel']:
+                continue
+
+            for file in product['files']:
+                if not file['isVrcUnitypackage']:
+                    continue
+                print("ee")
+                models.append({
+                    'name': file['name'],
+                    'file': file['path'],
+                    'version': file['version']
+                })
+                break
+
+        return models
